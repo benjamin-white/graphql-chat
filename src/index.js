efa124c0add29b5c2eeff86315f40046c8837fac
benjamin-white <benjaminwhite@live.co.uk>
2018-03-13 19:51:39 +0000

    adds sass build support and initial GraphQL setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,52 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-client-preset';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const wsLink = new WebSocketLink({
+
+  uri: 'wss://subscriptions.graph.cool/v1/cjeoymqbq52ft0163efovj7eq',
+  options: {
+    reconnect: true
+  }
+
+});
+
+const httpLink = new HttpLink({
+
+  uri: 'https://api.graph.cool/simple/v1/cjeoymqbq52ft0163efovj7eq'
+
+});
+
+const link = split(
+
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+
+);
+
+const client = new ApolloClient({
+
+  link,
+  cache: new InMemoryCache()
+
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
