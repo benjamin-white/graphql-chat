00985cdb01157a84c40808e6c790bd128b5dc45f
benjamin-white <benjaminwhite@live.co.uk>
2018-03-15 23:37:59 +0000

    formatting changes courtesy of Prettier

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
   state = {
     from: 'anonymous',
     content: ''
   };
 
   _createChat = async e => {
-    if (e.key === 'Enter') { // how xBrowser is this?
+    if (e.key === 'Enter') {
+      // how xBrowser is this?
       const { content, from } = this.state;
       await this.props.createChatMutation({
         variables: { content, from }
       });
       this.setState({ content: '' });
     }
-  }
+  };
 
   _subscribeToNewChats = _ => {
     this.props.allChatsQuery.subscribeToMore({
       document: gql`
         subscription {
-          Chat(filter: { mutation_in: [CREATED]}) {
+          Chat(filter: { mutation_in: [CREATED] }) {
             node {
               id
               from
@@ -50,7 +50,7 @@ class App extends Component {
         return result;
       }
     });
-  }
+  };
 
   componentDidMount() {
     const from = window.prompt('username');
@@ -62,9 +62,7 @@ class App extends Component {
     const allChats = this.props.allChatsQuery.allChats || [];
 
     return (
-
       <div className="">
-
         <div className="container">
           <h2>Chats</h2>
           {allChats.map(message => (
@@ -79,10 +77,8 @@ class App extends Component {
             placeholder="Start typing"
             onKeyPress={this._createChat}
           />
-
         </div>
       </div>
-
     );
   }
 }
@@ -111,5 +107,5 @@ const CREATE_CHAT_MUTATION = gql`
 
 export default compose(
   graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' }),
-  graphql(CREATE_CHAT_MUTATION, { name: 'createChatMutation'})
+  graphql(CREATE_CHAT_MUTATION, { name: 'createChatMutation' })
 )(App);
