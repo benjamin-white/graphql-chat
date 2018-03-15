b30c4b7ea2aec74cc6f1fb2f372f745e73662188
benjamin-white <benjaminwhite@live.co.uk>
2018-03-15 23:36:35 +0000

    adds text input and corresponding graphql mutation

--- README.md
+++
@@ -1,1 +0,0 @@
-+ Relay API: https://api.graph.cool/relay/v1/cjeoymqbq52ft0163efovj7eq
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Chatbox from './components/Chatbox';
@@ -8,28 +8,81 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   state = {
     from: 'anonymous',
     content: ''
   };
 
+  _createChat = async e => {
+    if (e.key === 'Enter') { // how xBrowser is this?
+      const { content, from } = this.state;
+      await this.props.createChatMutation({
+        variables: { content, from }
+      });
+      this.setState({ content: '' });
+    }
+  }
+
+  _subscribeToNewChats = _ => {
+    this.props.allChatsQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Chat(filter: { mutation_in: [CREATED]}) {
+            node {
+              id
+              from
+              content
+              createdAt
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newChatLinks = [
+          ...previous.allChats,
+          subscriptionData.data.Chat.node
+        ];
+        const result = {
+          ...previous,
+          allChats: newChatLinks
+        };
+        return result;
+      }
+    });
+  }
+
   componentDidMount() {
     const from = window.prompt('username');
     from && this.setState({ from });
+    this._subscribeToNewChats();
   }
 
   render() {
     const allChats = this.props.allChatsQuery.allChats || [];
 
     return (
+
       <div className="">
+
         <div className="container">
           <h2>Chats</h2>
           {allChats.map(message => (
             <Chatbox key={message.id} message={message} />
           ))}
+
+          {/* Message content input */}
+          <input
+            value={this.state.content}
+            onChange={e => this.setState({ content: e.target.value })}
+            type="text"
+            placeholder="Start typing"
+            onKeyPress={this._createChat}
+          />
+
         </div>
       </div>
+
     );
   }
 }
@@ -45,4 +98,18 @@ const ALL_CHATS_QUERY = gql`
   }
 `;
 
-export default graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' })(App);
+const CREATE_CHAT_MUTATION = gql`
+  mutation CreateChatMutation($content: String!, $from: String!) {
+    createChat(content: $content, from: $from) {
+      id
+      createdAt
+      from
+      content
+    }
+  }
+`;
+
+export default compose(
+  graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' }),
+  graphql(CREATE_CHAT_MUTATION, { name: 'createChatMutation'})
+)(App);
