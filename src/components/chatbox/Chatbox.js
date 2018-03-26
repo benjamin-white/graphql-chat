c3ce4c4ddb580c467c1bc46c974ed743df6ea875
benjamin-white <benjaminwhite@live.co.uk>
2018-03-26 22:30:14 +0100

    adds breakpoint to container and adopts suit-css

--- src/components/chatbox/Chatbox.js
+++ src/components/chatbox/Chatbox.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './Chatbox.css';
 
 const Chatbox = ({ message }) => (
-  <div className="chat-box">
-    <div className="chat-message">
-      <h5>{message.from}</h5>
-      <p>{message.content}</p>
-    </div>
+  <div className="ChatBox">
+    <h5>{message.from}</h5>
+    <p>{message.content}</p>
   </div>
 );
 
